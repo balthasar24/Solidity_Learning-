9d4d540c407a35ef7d5381ea4e6928866b3bf452
balthasar24 <balthasarhaw@gmail.com>
2025-12-26 15:02:50 +0700

    nambah struct, semacam class

--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -4,17 +4,31 @@ pragma solidity ^0.8.31;
 
 contract Twitter {
 
-    mapping(address => string[]) public tweets;
-
-    function createTweet(string memory a) public {
-        tweets[msg.sender].push(a);
+    struct Tweet{
+        address author;
+        string content;
+        uint256 timestamp;
+        uint256 likes;
     }
 
-    function getTweet(address a, uint i) public view returns (string memory) {
+    mapping(address => Tweet[]) public tweets;
+
+    function createTweet(string memory a) public {
+        Tweet memory newTweet = Tweet({
+            author: msg.sender,
+            content: a,
+            timestamp: block.timestamp,
+            likes: 0
+        });
+
+        tweets[msg.sender].push(newTweet);
+    }
+
+    function getTweet(address a, uint i) public view returns (Tweet memory) {
         return tweets[a][i];
     }
 
-    function getAllTweet(address a) public view returns (string[] memory) {
+    function getAllTweet(address a) public view returns (Tweet[] memory) {
         return tweets[a];
     }
 }
